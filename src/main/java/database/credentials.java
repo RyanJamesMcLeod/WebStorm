c93823f6c3c97ee70c9eabfcb85e3130287daddf
RyanJamesMcLeod <psystorm21@hotmail.com>
2015-03-10 14:14:44 -0400

    added database class and jquery link

---
+++ src/main/java/database/credentials.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.*;
+import java.util.logging.*;
+
+/**
+ *
+ * @author Ryan
+ */
+public class credentials {
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(credentials.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String hostname = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+        String portnum = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+        String user = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+        String pass = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+        String jdbc = "jdbc:mysql://" + hostname + ":" + portnum + "/sampleforum";
+        return DriverManager.getConnection(jdbc, user, pass);
+    }
+}
