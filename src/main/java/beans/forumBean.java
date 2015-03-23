fe252347bf31573c5f98d9e78057783c04e6d302
RyanJamesMcLeod <psystorm21@hotmail.com>
2015-03-23 08:36:18 -0400

    added forumBean

---
+++ src/main/java/beans/forumBean.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author Ryan
+ */
+@Stateful
+public class forumBean {
+    
+    String username = "";
+    String channel = "testchannel";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+    
+}
